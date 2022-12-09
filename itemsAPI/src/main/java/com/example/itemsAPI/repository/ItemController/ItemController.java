703844dc29da69f2f916f6f2e9a3085ce347aa4a
avrubio <ariadna.vanegas@gmail.com>
2022-12-09 12:14:07 -0500

    Item Controller class added and package folders fixed

---
+++ itemsAPI/src/main/java/com/example/itemsAPI/repository/ItemController/ItemController.java
@@ -0,0 +1,26 @@
+package com.example.itemsAPI.repository.ItemController;
+
+import com.example.itemsAPI.repository.entity.Item;
+import com.example.itemsAPI.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
+public class ItemController{
+
+    final ItemRepository itemRepository;
+
+
+    public ItemController(@Autowired ItemRepository itemRepository )
+    {
+        this.itemRepository = itemRepository;
+    }
+
+    @GetMapping
+    public Iterable<Item> getItems(){
+        return itemRepository.findAll();
+    }
+}
--- itemsAPI/src/main/java/com/example/itemsAPI/repository/entity/Item.java
+++ itemsAPI/src/main/java/com/example/itemsAPI/repository/entity/Item.java
@@ -1,4 +1,4 @@
-package com.example.itemsAPI.entity;
+package com.example.itemsAPI.repository.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;

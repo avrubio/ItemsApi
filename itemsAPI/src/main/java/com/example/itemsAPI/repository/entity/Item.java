dc8ecbb9d03bc3c7e78d7ea66165cfd8df815fbd
avrubio <ariadna.vanegas@gmail.com>
2022-12-09 12:00:54 -0500

    Task 8 update

--- itemsAPI/src/main/java/com/example/itemsAPI/repository/entity/Item.java
+++ itemsAPI/src/main/java/com/example/itemsAPI/repository/entity/Item.java
@@ -1,4 +1,70 @@
 package com.example.itemsAPI.repository.entity;
 
-public class Item {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String description;
+
+    private String price;
+
+    private String imageUrl;
+
+    public Integer getId()
+    {
+        return id;
+    }
+
+    public void setId( Integer id )
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName( String name )
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription( String description )
+    {
+        this.description = description;
+    }
+
+    public String getImageUrl()
+    {
+        return imageUrl;
+    }
+
+    public void setImageUrl( String imageUrl )
+    {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
